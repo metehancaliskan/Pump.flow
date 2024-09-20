d9d06c392d6bddedf5af954527749320d973fe2f
metehan@knidos.xyz <metehan@knidos.xyz>
2024-09-21 05:35:56 +0800

    adding liq pool function

--- contracts/src/Token.sol
+++ contracts/src/Token.sol
@@ -4,8 +4,17 @@ pragma solidity ^0.8.26;
 import "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
 
 contract Token is ERC20{
+
+    address public admin;
+
     constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
         _mint(msg.sender, initialSupply);
+        admin = msg.sender;
+    }
+
+    function mint(address to, uint256 amount) external {
+        require(msg.sender == admin, "Only TokenFactory can mint tokens");
+        _mint(to, amount);
     }
 
 
--- contracts/src/TokenFactory.sol
+++ contracts/src/TokenFactory.sol
@@ -4,15 +4,18 @@ pragma solidity ^0.8.26;
 import "./Token.sol";
 
 contract TokenFactory {
-    uint256 constant public DECIMALS = 10**18;
-    uint256 constant public MAX_SUPPLY = 10**9 * DECIMALS;
-    uint256 constant public initialSupply = MAX_SUPPLY*20/100;
-    uint256 constant public k = 23437500;
-    uint256 constant public offset = 9375*10**13;
-    uint256 constant public SCALING_FACTOR = 10**30;
+    uint constant public DECIMALS = 10**18;
+    uint constant public MAX_SUPPLY = 10**9 * DECIMALS;
+    uint constant public initialSupply = MAX_SUPPLY*20/100;
+    uint constant public k = 23437500;
+    uint constant public offset = 9375*10**13;
+    uint constant public SCALING_FACTOR = 10**30;
+    uint constant public FUNDING_GOAL = 150000 ether;
 
 
     mapping(address => bool) public tokens;
+    mapping(address => uint) public colletral; // amount of Flow recieved for buying tokens
+    mapping(address => mapping(address => uint)) public balances;
 
     event TokenCreated(address tokenAddress, string name, string symbol);
 
@@ -29,6 +32,12 @@ contract TokenFactory {
         uint availableSupply = MAX_SUPPLY - initialSupply - token.totalSupply();
         require(availableSupply >= amount, "Not enough tokens available");
         uint requiredFlow = calculateRequiredFlow(tokenAddress, amount);
+        require(msg.value >= requiredFlow, "Not enough FLOW");
+        colletral[tokenAddress] += requiredFlow;
+        balances[tokenAddress][msg.sender] += amount;
+        token.mint(address(this), amount);
+        if (colletral[tokenAddress] >= FUNDING_GOAL) {
+        }
     }
 
     function calculateRequiredFlow(address tokenAddress, uint256 amount) public returns (uint) {
@@ -39,6 +48,10 @@ contract TokenFactory {
         uint fa = k*a + offset;
         uint fb = k*b + offset;
         return (b-a)*(fa+fb)/(2*SCALING_FACTOR);
+    }
+
+    function _createLiquiditynPool(address tokenAddress) external {
+        Token token = Token(tokenAddress);
     }
 
 
