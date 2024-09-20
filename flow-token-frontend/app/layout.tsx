5db3bc7abda7d526a79815c379053ffb4a7eca3d
Bertan Köfön <bertankofon@gmail.com>
2024-09-21 05:40:08 +0800

    update the button activity

--- flow-token-frontend/app/layout.tsx
+++ flow-token-frontend/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import TanstackProvider from "@/components/providers/tanstack-provider";
+import { Web3Provider } from "@/components/providers/web3-provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,9 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TanstackProvider>
-          {children}
-        </TanstackProvider>
+        <Web3Provider>{children}</Web3Provider>
       </body>
     </html>
   );
