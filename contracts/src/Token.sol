e37b521d23cee7cd9841a79a024293b7ec8d7a3b
Bertan Köfön <bertankofon@gmail.com>
2024-09-21 16:13:29 +0800

    new contract logic

--- contracts/src/Token.sol
+++ contracts/src/Token.sol
@@ -1,22 +1,22 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.26;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
-import "@openzeppelin-contracts-5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Token is ERC20{
+contract Token is ERC20 {
 
-    address public admin;
+    address public owner;
 
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
-        admin = msg.sender;
+    constructor(string memory name, string memory symbol, uint initialMintValue) ERC20(name, symbol) {
+        _mint(msg.sender, initialMintValue);
+        owner = msg.sender;
     }
 
-    function mint(address to, uint256 amount) external {
-        require(msg.sender == admin, "Only TokenFactory can mint tokens");
-        _mint(to, amount);
+    function mint(uint mintQty, address receiver) external returns(uint){
+        require(msg.sender == owner, "Mint can only be called by the owner");
+        _mint(receiver, mintQty);
+        return 1;
     }
-
 
 
 }
