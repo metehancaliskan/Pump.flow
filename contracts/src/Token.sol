0137cea4d1f6f5992d5126b1dd40f23c2ae0ccfd
metehan@knidos.xyz <metehan@knidos.xyz>
2024-09-22 05:28:49 +0800

    burn function

--- contracts/src/Token.sol
+++ contracts/src/Token.sol
@@ -12,11 +12,39 @@ contract Token is ERC20 {
         owner = msg.sender;
     }
 
+    /**
+     * @notice Mints new tokens and sends them to the specified receiver.
+     * @dev Only the owner of the contract can call this function.
+     * @param mintQty The amount of tokens to mint.
+     * @param receiver The address that will receive the minted tokens.
+     * @return Returns 1 to indicate success.
+     */
     function mint(uint mintQty, address receiver) external returns(uint){
         require(msg.sender == owner, "Mint can only be called by the owner");
         _mint(receiver, mintQty);
         return 1;
     }
 
+    /**
+     * @notice Burns a specified amount of tokens from the caller's account.
+     * @param burnQty The amount of tokens to burn.
+     * @return Returns 1 to indicate success.
+     */
+    function burn(uint burnQty) external returns(uint) {
+        _burn(msg.sender, burnQty);
+        return 1;
+    }
 
+    /**
+     * @notice Burns a specified amount of tokens from a specified address.
+     * @dev Only the owner of the contract can call this function.
+     * @param burnQty The amount of tokens to burn.
+     * @param account The address from which tokens will be burned.
+     * @return Returns 1 to indicate success.
+     */
+    function burnFrom(uint burnQty, address account) external returns(uint) {
+        require(msg.sender == owner, "Burn from can only be called by the owner");
+        _burn(account, burnQty);
+        return 1;
+    }
 }
